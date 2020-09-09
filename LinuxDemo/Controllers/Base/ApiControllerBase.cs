521c9e545fc547d52c947a9d01a1a50c5f0cf2e4
RvSoul <825728964@qq.com>
2020-09-09 13:34:19 +0800

    特意增加了发布linux的api

---
+++ LinuxDemo/Controllers/Base/ApiControllerBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LinuxDemo.ApiAttribute;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LinuxDemo.Controllers.Base
+{
+    /// <summary>
+    /// /
+    /// </summary> 
+    [CommonApiException]
+    public class ApiControllerBase : ControllerBase
+    {
+
+    }
+}
